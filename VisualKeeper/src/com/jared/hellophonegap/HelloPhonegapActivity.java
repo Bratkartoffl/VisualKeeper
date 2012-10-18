5dc191d2f09ef6742366bb44ec7e6c66f62d4901
Abel Sanchez <bossbiga123@gmail.com>
2012-10-18 12:21:18 -0600

    Commit 1
    Added Splash Screen

--- VisualKeeper/src/com/jared/hellophonegap/HelloPhonegapActivity.java
+++ VisualKeeper/src/com/jared/hellophonegap/HelloPhonegapActivity.java
@@ -1,6 +1,5 @@
 package com.jared.hellophonegap;
 
-import android.app.Activity;
 import android.os.Bundle;
 import org.apache.cordova.*;
 public class HelloPhonegapActivity extends DroidGap {
@@ -8,6 +7,7 @@ public class HelloPhonegapActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
         super.loadUrl("file:///android_asset/www/index.html");
     }
 }
