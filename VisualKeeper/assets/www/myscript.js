bfa8ed67bc27f0e0a916da6603aee4fdaba06a81
Jared Cahalan <jcahalan@mavs.coloradomesa.edu>
2012-10-19 01:10:50 -0600

    Finished blocking out new task page
    The new task page is finished except for a bug in the date and time
    picker. I might have to just make our own widget.

--- VisualKeeper/assets/www/myscript.js
+++ VisualKeeper/assets/www/myscript.js
@@ -3,7 +3,17 @@ var counter=1;
 function capturePhoto(){
 	navigator.camera.getPicture(showPhoto,null,{sourceType:1,quality:60});
 }
-
+function init(){
+	$('#newTask').bind('pagebeforeshow',resetNewTask);
+	$('#newTask').bind('pageinit',initNewTask)
+}
+function initNewTask(){
+	$('#cancelButton').bind('tap',cancelNewTask);
+}
+function cancelNewTask(){
+	resetNewTask();
+	$.mobile.changePage($('#home'));
+}
 function showPhoto(data){
 	$('#taskPic').attr('src', data);
 	
@@ -22,21 +32,28 @@ function addPhoto(data){
 	$('#exPhoto1').attr('src',data)
 	$('#extraPhotos').listview('refresh');
 }
-function init(){
+function resetNewTask(){
 	counter=1;
+	var s='';
+	$('#extraPhotos').html(s).listview('refresh');
+	console.log("inited");
 }
 function addPhotoSpace(){
-	var fieldName = "nameField"+counter,
+	var list = $('#extraPhotos'),
+		fieldName = "nameField"+counter,
 		fieldDesc = "descField"+counter,
 		photoDesc = "descPhoto"+counter,
 		photoName = "namePhoto"+counter,
 		photoId = "exPhoto"+counter,
-		html = '<li><img id="' + photoId + '" src="camera.jpg" >';
+		html = '<li data-role="fieldcontain" data-theme="a">';
 	html += '<label for="'+fieldName+'">Photo Name</label>';
-	html += '<input type="text" name="'+photoName+'" id="'+fieldName+'" value="" /></br>';
+	html += '<input type="text" name="'+photoName+'" id="'+fieldName+'" />';
 	html += '<label for="'+fieldDesc+'">Photo Desc.</label>';
-	html += '<input type="text" name="'+photoDesc+'" id="'+fieldDesc+'" value="" /></li>';
-	$('#extraPhotos').append(html).listview('refresh');
-	$(photoId).bind('tap', captureAdditionalPhoto);
+	html += '<input type="text" name="'+photoDesc+'" id="'+fieldDesc+'" /><img id="' + photoId + '" src="camera.jpg" style="border-radius:5px; margin-left:8px; margin-top:7px;"></li>';
+	
+	list.append(html).listview("refresh");
+	$('#newTaskContent').trigger("create");
+	$('#'+photoId).bind('tap', captureAdditionalPhoto);
 	counter++;
+	
 }
